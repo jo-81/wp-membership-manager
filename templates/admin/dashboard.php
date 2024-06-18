30e5b92537c618e0ecd05d3b5c80e22677e6ebd7
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2024-06-18 13:06:58 +0200

    role (#3) : remove a role added

--- templates/admin/dashboard.php
+++ templates/admin/dashboard.php
@@ -1,5 +1,9 @@
-<?php 
-    $success = Inc\Message::get('wp_mm_success');
+<?php
+
+    use Wp_Membership_Manager\Model\WP_MM_Role_Model;
+
+    $message = Inc\Message::get('wp_mm_message_role');
+    $roles = (new WP_MM_Role_Model)->get_roles();
 ?>
 
 <section>
@@ -7,8 +11,8 @@
         <h1><?php echo esc_html(get_admin_page_title()) ?></h1>
     </header>
 
-    <?php if ($success) : ?>
-        <p><?php esc_html_e($success['message'], "wp_membership_manager"); ?></p>
+    <?php if ($message) : ?>
+        <p><?php esc_html_e($message['message'], "wp_membership_manager"); ?></p>
     <?php endif; ?>
 
     <section>
@@ -20,15 +24,37 @@
         </p>
 
         <div>
-            <?php foreach(array_reverse(wp_roles()->roles) as $role => $caps) : ?>
 
-                <?php if(! preg_match("#wp_mm_#", $role)) : continue; endif; ?>
+            <?php if (empty($roles)) : ?>
+                <p><?php esc_html_e("No roles registered", "wp-membership-manager") ?></p>
+            <?php endif; ?>
 
-                <p><?php echo ucfirst($caps['name']); ?></p>
+            <?php foreach(array_reverse($roles) as $role) : ?>
+
+                <p><?php echo ucfirst($role->display_name); ?></p>
                 <p><a href="<?php echo esc_url(add_query_arg([
                     'page' => 'wp_mm_roles',
                     'role_name' => $role,
                 ], admin_url("admin.php"))) ?>">Consulter</a></p>
+
+                <?php add_thickbox(); ?>
+                <div id="ww-mm-remove-role" style="display:none;">
+                    <form method="POST" action="<?php echo esc_url(admin_url("admin-post.php")); ?>">
+
+                        <input type="hidden" name="action" value="wp_mm_remove_role">
+                        <input type="hidden" name="wp_mm_role" value="<?php echo esc_attr($role->name); ?>">
+
+                        <?php wp_nonce_field("wp_mm_action_remove_role", "wp_mm_nonce_remove_role"); ?>
+
+                        <p><?php esc_html_e("Do you want delete this role ?", "wp-membership-manager") ?></p>
+                        <p>
+                            <button tyoe="submit"><?php esc_html_e("Remove", "wp-membership-manager"); ?></button>
+                        </p>
+                    </form>
+                </div>
+
+                <a href="#TB_inline?&width=600&height=120&inlineId=ww-mm-remove-role" class="thickbox"><?php esc_html_e("Remove", "wp-membership-manager"); ?></a>
+
             <?php endforeach; ?>
         </div>
     </section>
